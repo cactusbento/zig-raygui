e94bc948b33998ae2f4eeb14dcc78b4036ef8d82
cactusbento <cactusbento@outlook.com>
2023-06-25 14:45:23 -0400

    Updated to zig-0.11.0-38XX

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A WIP binding for raygui for zig.
 
-Will turn into a library in future using `build.zig` after zig-0.11.0, but for now, I'll just stick with the current setup.
+Will turn into a library in future using `build.zig`, but for now, I'll just stick with the current setup.
 
 You must provide the necessary setup to include both `raylib.h` and `raygui.h` in `raygui.zig`.
 This can be done by using `exe.linkSystemLibrary([]const u8)`, `exe.addIncludePath([]const u8)`, or just adding the headers next to `raygui.zig`.
--- build.zig
+++ build.zig
@@ -1,26 +1,50 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable("zigRL", "src/main.zig");
-
-    // Add C headers
-    exe.addIncludePath("lib/headers");
-
+    const exe = b.addExecutable(.{
+        .name = "zigRayGuiBindings",
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     exe.linkLibC();
+    exe.addIncludePath("lib/headers");
     exe.linkSystemLibrary("raylib");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.install();
+    b.installArtifact(exe);
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    // Creates a step for unit testing. This only builds the test executable
+    // but does not run it.
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
 }
