753549e8b6c1e0d974d5a20e1c08350bdb7f230e
cactusbento <cactusbento@outlook.com>
2023-06-30 11:54:11 -0400

    Added GuiToggle as Toggle

--- src/controls/elements.zig
+++ src/controls/elements.zig
@@ -130,3 +130,48 @@ pub const Checkbox = struct {
         self.rect.height = h;
     }
 };
+
+/// The basic Raylib toggle.
+pub const Toggle = struct {
+    const Self = @This();
+
+    rect: c.Rectangle,
+    name: []const u8,
+
+    // Checkbox state
+    value: bool = false,
+
+    pub fn init(name: []const u8, rect: Rect) Self {
+        return .{
+            .name = name,
+            .rect = .{
+                .x = rect.x,
+                .y = rect.y,
+                .width = rect.width,
+                .height = rect.height,
+            },
+        };
+    }
+
+    pub fn draw(self: *Self) void {
+        const prev_val = self.value;
+
+        if (c.GuiCheckBox(
+            self.rect,
+            @as([*c]const u8, self.name.ptr),
+            &self.value,
+        ) != 0 and prev_val) {
+            self.value = !self.value;
+        }
+    }
+
+    pub fn move(self: *Self, x: f32, y: f32) void {
+        self.rect.x = x;
+        self.rect.y = y;
+    }
+
+    pub fn resize(self: *Self, w: f32, h: f32) void {
+        self.rect.width = w;
+        self.rect.height = h;
+    }
+};
